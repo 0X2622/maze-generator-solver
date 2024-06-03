f83d5148544585f4f93d76b24fbfbc7ed9748720
0x2622 <kev_olsson@hotmail.com>
2024-06-03 18:13:55 +0200

    deleting comments in main.cpp

--- maze/main.cpp
+++ maze/main.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
 #include "menu.h"
 
-//NEXT: Städa Resten av mazegenerator - DFS + findNeighboors
-
-//the purpose of the main() function is to just start/boot up the program, and then initate a menu object which will
-//load up the application start screen and menu choices. 
-// The object 'menu' is what will navigate the entire flow of the program, by providing a an initial visual interface,
-// and communication with other classes that lets the user to use the program effectively.
-
-//ToDo::
-
-//Coding conventions att fixa:
-//1 : Se till att allt är camelcase med första bokstaven som lowerCase.
-//2: Se till att ha en abstrakt förklaring till varje funktion, och överlåt detaljerna till mig/de som är intresserade.
-//3: se till att alla funktionsnamn och variabelnamn beskriver deras syfte på ett intuitivt sätt
-//3:Städa bort alla stora kommentarer, och lägg på minimalistiska självförklarande kommentarer för hela koden.
-//4: Skriv in namn, datum, osv på varje fil, komprimera och lämna in
-
-// kolla att mina includes makes sense, se om de någon include jag inte behöver
-
-
 int main() {
 
 	bool running = true; //boolean value that indicates that the program is running
